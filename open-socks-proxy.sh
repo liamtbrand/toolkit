91c08d42425c8512259f2bae43c73c2eac3fe45d
Liam Brand <liamtbrand@gmail.com>
2022-11-26 12:59:28 -0800

    Updates

---
+++ open-socks-proxy.sh
@@ -0,0 +1,2 @@
+# ssh -D localhost:9999 frozen@bayview.liamtbrand.com -p 25566
+echo "$ ssh -D localhost:9999 user@target -p 22"
--- rsync-backup.sh
+++ rsync-backup.sh
@@ -1,1 +1,12 @@
 # rsync -naruvz --rsh='ssh -p25566' src frozen@bayview.liamtbrand.com:/home/LaCie/dest
+
+rsync \
+    --dry-run \
+    --archive \
+    --compress \
+    --rsh='ssh -p25567' \
+    --human-readable \
+    --progress \
+    --exclude=wikipedia_en_all_maxi_2020-11.zim \
+    "$(pwd)" \
+    frozen@bayview.liamtbrand.com:/home/LaCie/
