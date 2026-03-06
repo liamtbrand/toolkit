949e5ba91de0b67bea2463fa6c7c0da2e433e38c
Liam T. Brand <liamtbrand@gmail.com>
2026-03-06 17:07:05 +1300

    Add useful scripts

--- scripts/scan_tcp_ports.sh
+++ scripts/scan_tcp_ports.sh
@@ -3,9 +3,12 @@ set -euo pipefail
 
 source "$(dirname "$0")/assert_macos.sh"
 
-TARGET="$1"
-START_PORT=1
-END_PORT=65535
+TARGET="${1:-localhost}"
+START_PORT="${2:-1}"
+END_PORT="${3:-65535}"
+
+echo "Port scan script for scanning TCP ports."
+echo "Defaults to localhost. You can specify the target like: scan_tcp_ports.sh <ip> <startport> <endport>"
 
 echo "Scanning $TARGET ports $START_PORT to $END_PORT..."
 
